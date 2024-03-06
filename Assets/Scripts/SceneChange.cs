e0941e185d963d8e86f52b568b0309597f4587d5
Kaan Kara <kaan.kara@yahoo.com>
2024-03-06 08:48:47 +0300

    Bug fixes

--- Assets/Scripts/SceneChange.cs
+++ Assets/Scripts/SceneChange.cs
@@ -9,4 +9,9 @@ public class SceneChange : MonoBehaviour
     {
         SceneManager.LoadScene("MainScene");
     }
+
+    public void LoadCaptchaScene()
+    {
+        SceneManager.LoadScene("CaptchaScene");
+    }
 }
