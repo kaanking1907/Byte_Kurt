c7f807a78f0ade06fd32253a075ae86915d8cbb4
Kaan Kara <kaan.kara@yahoo.com>
2024-03-09 16:14:27 +0300

    weather script

---
+++ Assets/Scripts/WeatherPage.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeatherPage : MonoBehaviour
+{
+
+    string API = "6752731580e85d07d0a62fcd56683136";
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
