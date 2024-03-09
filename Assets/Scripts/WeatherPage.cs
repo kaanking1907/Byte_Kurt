54b1f9ba439299d9836177a8e55a075abcd7e3db
Kaan Kara <kaan.kara@yahoo.com>
2024-03-09 16:20:22 +0300

    Update WeatherPage.cs

--- Assets/Scripts/WeatherPage.cs
+++ Assets/Scripts/WeatherPage.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class WeatherPage : MonoBehaviour
 {
 
-    string API = "6752731580e85d07d0a62fcd56683136";
+    //string API = "6752731580e85d07d0a62fcd56683136";
     // Start is called before the first frame update
     void Start()
     {
